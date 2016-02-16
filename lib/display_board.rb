d99e7d6fe61a8db62f9abcc97d833e2757257653
justinhamlin <justin.hamlin@gmail.com>
2016-02-16 01:26:34 +0000

    nitrous-backup at Tue Feb 16 01:26:34 UTC 2016

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,19 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  board = ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "]
+  puts  "   |   |   "
+  puts  "-----------"
+  puts  "   |   |   "
+  puts  "-----------"
+  puts  "   |   |   "
+end
+
+def display_board(board)
+board = ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "]
+  puts  " O |   |   "
+  puts  "-----------"
+  puts  "   | X |   "
+  puts  "-----------"
+  puts  "   |   |   "
+end
